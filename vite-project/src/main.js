590ea21b4618e2fbb7b304764e73c52b35e4ae88
hesaneprh <hesanepirhadi8@gmail.com>
2026-01-30 21:23:37 +0330

    v1

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,1 +1,13 @@
-import './style.css'
+const sidebar=document.getElementById ("sidebar");
+const button= document.getElementById ("btn_sidebar");
+const cross=document.getElementById ("close");
+
+console.log(sidebar);
+
+button.addEventListener("click" , () => {
+    sidebar.classList.toggle("show_sidebar");
+
+})
+cross.addEventListener("click" , ( ) => {
+    sidebar.classList.remove("show_sidebar");
+})
